65e6951f681b3e2841ff90dea5407fc015472c71
Abhi24001 <101787857+Abhi24001@users.noreply.github.com>
2022-12-27 09:54:07 +0530

    Update Maximum Tastiness of Candy Basket.cpp

--- Leet-code-contest 325/Maximum Tastiness of Candy Basket.cpp
+++ Leet-code-contest 325/Maximum Tastiness of Candy Basket.cpp
@@ -1,19 +1,42 @@
-int maximumTastiness(vector<int>& price, int k) {
-    sort(price.begin(), price.end());
-    int lo = 0, hi = 1e9;
-    while (lo < hi) {
-        int mid = lo + (hi - lo) / 2;
-        if (check(mid, price, k)) lo = mid + 1;
-        else hi = mid;
+class Solution
+{
+public:
+
+    bool isValid(int minDiff, vector<int>& price, int k) {
+        int n = price.size();
+        int cnt = 1;
+
+        int prev = price[0];
+
+        for(int i = 1; i < n; i++) {
+            if(price[i] - prev >= minDiff) {
+                cnt++;
+                prev = price[i];
+            }
+        }
+
+        if(cnt >= k) return true;
+
+        return false;
+
     }
-    return lo - 1;
-}
-bool check(int x, vector<int>& price, int k) {
-    int last = price[0], count = 1, i = 1;
-    while (count < k && i < price.size()) {
-        if (price[i] - last >= x)
-            last = price[i], count++;
-        i++;
+    int maximumTastiness(vector<int> &price, int k) {
+        sort(price.begin(), price.end());
+        int n = price.size();
+
+        int st = 0;
+        int en = price[n - 1] - price[0];
+
+        int ans = -1;
+
+        while(st <= en) {
+            int mid = st + (en - st) / 2;
+
+            if(isValid(mid, price, k)) ans = mid, st = mid + 1;
+            else en = mid - 1; 
+        }
+
+        return ans;
     }
-    return count == k;
-}
+};
+// upvote if you like the solution
